cbc5b6c430b13600c77bb6b71af61b69d0b5ba6f
Daniel Andreas Wang <danielaw@getacademy.no>
2025-12-02 02:05:58 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Card from './components/Card.vue';
+</script>
+
+<template>
+  <Card rank="ace" suit="hearts" />
+  <Card rank="king" suit="hearts" />
+  <Card rank="queen" suit="hearts" />
+  <Card rank="jack" suit="hearts" />
+  <Card rank="10" suit="hearts" />
+</template>
---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  suit: String,
+  rank: String
+});
+</script>
+
+<template>
+    <img :src="`/svgs/${rank}_of_${suit}.svg`" :alt="`${rank} of ${suit}`" />
+</template>
+
+<style>
+img {
+  width: 90px;
+  height: auto;
+}
+</style>
