dd51a7b473b29479b5d4d2395f09ede4d4e9aa27
Daniel Andreas Wang <danielaw@getacademy.no>
2025-12-02 16:32:23 +0100

    add onMounted import

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import Card from './components/Card.vue';
 import { useGameStore } from './stores/gameStore';
+import { onMounted } from 'vue';
 
 const game = useGameStore();
 
 onMounted(() => {
   game.initializeDeck();
+  game.setPlayerCount(4);
+  game.dealHands(13);
 });
 </script>
 
